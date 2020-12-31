6d5ad2d85f784fb27dfa81260078ae76758f9b97
lbluque <lbluque@berkeley.edu>
2020-12-30 18:15:44 -0800

    update regression __init__.py for direct imports

--- theorytoolkit/regression/__init__.py
+++ theorytoolkit/regression/__init__.py
@@ -1,6 +1,16 @@
 """Contains classes to fit and modify fits of Cluster Expansions."""
 
+from .lasso import GroupLasso, SparseGroupLasso, AdaptiveLasso, \
+    AdaptiveGroupLasso, AdaptiveSparseGroupLasso
 from .wdr_lasso import WDRLasso
 from .utils import constrain_dielectric
 
-__all__ = ['WDRLasso', 'constrain_dielectric']
+__all__ = [
+    'GroupLasso',
+    'SparseGroupLasso',
+    'AdaptiveLasso',
+    'AdaptiveGroupLasso',
+    'AdaptiveSparseGroupLasso',
+    'WDRLasso',
+    'constrain_dielectric'
+]
