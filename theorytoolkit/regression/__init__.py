7f826c44dda7db46880f5ac14c9838f74b13f32f
lbluque <lbluque@berkeley.edu>
2021-08-03 18:26:34 -0700

    changed __init__.py

--- theorytoolkit/regression/__init__.py
+++ theorytoolkit/regression/__init__.py
@@ -2,7 +2,6 @@
 from .ols import OrdinaryLeastSquares
 from .lasso import Lasso
 from .mixedL0 import L1L0, L2L0
-from .l2l0 import L2L0Estimator
 
 from .lasso import GroupLasso, SparseGroupLasso, AdaptiveLasso, \
     AdaptiveGroupLasso, AdaptiveSparseGroupLasso
