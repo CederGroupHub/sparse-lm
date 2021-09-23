56daa91ae2574bf1e44e8046abf0d653078ba044
lbluque <lbluque@berkeley.edu>
2021-09-22 20:39:12 -0700

    first implementation of overlap group lasso (needs further checking)

--- theorytoolkit/regression/lasso.py
+++ theorytoolkit/regression/lasso.py
@@ -1,5 +1,6 @@
 """A set of generalized lasso estimators.
 * Group Lasso
+* Overlap Group Lasso
 * Sparse Group Lasso
 * Adaptive Lasso
 * Adaptive Group Lasso
@@ -178,7 +179,6 @@ class GroupLasso(Lasso):
     Where G represents groups of features/coefficients
     """
 
-    # TODO set groups by list of indices to allow overlap
     def __init__(self, groups, alpha=1.0, group_weights=None,
                  fit_intercept=False, normalize=False, copy_X=True,
                  warm_start=False, solver=None, **kwargs):
@@ -241,6 +241,86 @@ class GroupLasso(Lasso):
         objective = 1 / (2 * X.shape[0]) * cp.sum_squares(X @ self._beta - y) \
             + self._alpha * (self.group_weights @ grp_reg)
         return objective
+
+
+class OverlapGroupLasso(GroupLasso):
+    """Overlap Group Lasso implementation.
+
+    Regularized model:
+        || X * Beta - y ||^2_2 + alpha * \sum_{G} w_G * ||Beta_G||_2
+    Where G represents groups of features/coefficients, and overlaping groups
+    are acceptable. Meaning a coefficients can be in more than one group.
+    """
+
+    def __init__(self, groups, alpha=1.0, group_weights=None,
+                 fit_intercept=False, normalize=False, copy_X=True,
+                 warm_start=False, solver=None, **kwargs):
+        """Initialize estimator.
+
+        Args:
+            groups (list of lists):
+                list of lists of integers specifying groups. The length of the
+                list holding lists should be the same as model. Each inner list
+                has integers specifying the groups the coefficient for that
+                index belongs to. i.e. [[1,2],[2,3],[1,2,3]] means the first
+                coefficient belongs to group 1 and 2, the second to 2, and 3
+                and the third to 1, 2 and 3. In other words the 3 groups would
+                be: (0, 2), (0, 1, 2), (1, 2)
+            alpha (float):
+                Regularization hyper-parameter.
+            fit_intercept (bool):
+                Whether the intercept should be estimated or not.
+                If False, the data is assumed to be already centered.
+            group_weights (ndarray): optional
+                Weights for each group to use in the regularization term.
+                The default is to use the sqrt of the group sizes, however any
+                weight can be specified. The array must be the
+                same length as the number of different groups given.
+                If you need all groups weighted equally just pass an array of
+                ones.
+            normalize (bool):
+                This parameter is ignored when fit_intercept is set to False.
+                If True, the regressors X will be normalized before regression
+                by subtracting the mean and dividing by the l2-norm.
+                If you wish to standardize, please use StandardScaler before
+                calling fit on an estimator with normalize=False
+            copy_X (bool):
+                If True, X will be copied; else, it may be overwritten.
+            warm_start (bool):
+                When set to True, reuse the solution of the previous call to
+                fit as initialization, otherwise, just erase the previous
+                solution.
+            solver (str):
+                cvxpy backend solver to use. Supported solvers are:
+                ECOS, ECOS_BB, CVXOPT, SCS, GUROBI, Elemental.
+                GLPK and GLPK_MI (via CVXOPT GLPK interface)
+            **kwargs:
+                Kewyard arguments passed to cvxpy solve.
+                See docs linked in CVXEstimator base class for more info.
+        """
+        self.overlap_groups = groups
+        self.group_ids = np.unique([gid for grp in groups for gid in grp])
+        self.group_ids.sort()
+        beta_indices = [[i for i, grp in enumerate(groups) if grp_id in grp]
+                        for grp_id in self.group_ids]
+        extended_groups = np.concatenate(
+            [len(g) * [i, ] for i, g in enumerate(beta_indices)])
+
+        self.beta_indices = np.concatenate(beta_indices)
+        super().__init__(
+            extended_groups, alpha=alpha, group_weights=group_weights,
+            fit_intercept=fit_intercept, normalize=normalize, copy_X=copy_X,
+            warm_start=warm_start, solver=solver, **kwargs)
+
+    def _solve(self, X, y, *args, **kwargs):
+        """Solve the cvxpy problem."""
+        problem = self._get_problem(X[:, self.beta_indices], y)
+        problem.solve(solver=self.solver, warm_start=self.warm_start,
+                      **self.solver_opts)
+        beta = np.array(
+            [sum(self._beta.value[self.beta_indices == i])
+             for i in range(X.shape[1])])
+        return beta
 
 
 class SparseGroupLasso(GroupLasso):
