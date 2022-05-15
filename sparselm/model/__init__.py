364bd8784c06415f1075a141d302cfc73fdd8251
lbluque <lbluque@berkeley.edu>
2022-05-15 12:51:56 -0700

    refactor mixed l0 from regularizedl0 base

--- sparselm/model/__init__.py
+++ sparselm/model/__init__.py
@@ -3,7 +3,7 @@
 from sparselm.model.ols import OrdinaryLeastSquares
 from sparselm.model.lasso import Lasso
 from sparselm.model.miqp.best_subset import BestSubsetSelection
-from sparselm.model.miqp.regularized_l0 import L1L0, L2L0
+from sparselm.model.miqp.regularized_l0 import L1L0, L2L0, RegularizedL0
 from sparselm.model.lasso import Lasso, GroupLasso, OverlapGroupLasso, SparseGroupLasso
 from sparselm.model.adaptive_lasso import AdaptiveLasso, AdaptiveGroupLasso, \
     AdaptiveOverlapGroupLasso, AdaptiveSparseGroupLasso
@@ -12,6 +12,7 @@ __all__ = [
     "OrdinaryLeastSquares",
     "Lasso",
     "BestSubsetSelection",
+    "RegularizedL0",
     "L1L0",
     "L2L0",
     "GroupLasso",
--- sparselm/model/miqp/regularized_l0.py
+++ sparselm/model/miqp/regularized_l0.py
@@ -19,8 +19,112 @@ from cvxpy.atoms.affine.wraps import psd_wrap
 from sparselm.model.base import CVXEstimator
 
 
-# TODO make RegularizedL0 as a base class and then derive
-class MixedL0(CVXEstimator, metaclass=ABCMeta):
+class RegularizedL0(CVXEstimator):
+    """Implementation of l0 regularized estimator.
+    """
+    def __init__(self, alpha=1.0, big_M=1000, hierarchy=None, ignore_psd_check=True,
+                 fit_intercept=False, normalize=False, copy_X=True, warm_start=False,
+                 solver=None, **kwargs):
+        """
+        Args:
+            alpha (float):
+                Regularization hyper-parameter.
+            big_M (float):
+                Upper bound on the norm of coefficients associated with each
+                cluster (groups of coefficients) ||Beta_c||_2
+            hierarchy (list):
+                A list of lists of integers storing hierarchy relations between
+                coefficients.
+                Each sublist contains indices of other coefficients
+                on which the coefficient associated with each element of
+                the list depends. i.e. hierarchy = [[1, 2], [0], []] mean that
+                coefficient 0 depends on 1, and 2; 1 depends on 0, and 2 has no
+                dependence.
+            ignore_psd_check (bool):
+                Wether to ignore cvxpy's PSD checks  of matrix used in quadratic
+                form. Default is True to avoid raising errors for poorly
+                conditioned matrices. But if you want to be strict set to False.
+            fit_intercept (bool):
+                Whether the intercept should be estimated or not.
+                If False, the data is assumed to be already centered.
+            normalize (bool):
+                This parameter is ignored when fit_intercept is set to False.
+                If True, the regressors X will be normalized before regression
+                by subtracting the mean and dividing by the l2-norm.
+                If you wish to standardize, please use StandardScaler before
+                calling fit on an estimator with normalize=False
+            copy_X (bool):
+                If True, X will be copied; else, it may be overwritten.
+            warm_start (bool):
+                When set to True, reuse the solution of the previous call to
+                fit as initialization, otherwise, just erase the previous
+                solution.
+            solver (str):
+                cvxpy backend solver to use. Supported solvers are:
+                ECOS, ECOS_BB, CVXOPT, SCS, GUROBI, Elemental.
+                GLPK and GLPK_MI (via CVXOPT GLPK interface)
+            **kwargs:
+                Kewyard arguments passed to cvxpy solve.
+                See docs linked above for more information.
+        """
+        super().__init__(fit_intercept=fit_intercept, normalize=normalize,
+                         copy_X=copy_X, warm_start=warm_start, solver=solver,
+                         **kwargs)
+
+        self.hierarchy = hierarchy
+        self._alpha = alpha
+        self._lambda0 = cp.Parameter(nonneg=True, value=alpha)
+        self._big_M = cp.Parameter(nonneg=True, value=big_M)
+        self.ignore_psd_check = ignore_psd_check
+        self._z0 = None
+
+    @property
+    def alpha(self):
+        return self._alpha
+
+    @alpha.setter
+    def alpha(self, val):
+        self._alpha = val
+        self._lambda0.value = val
+
+    @property
+    def big_M(self):
+        return self._big_M.value
+
+    @big_M.setter
+    def big_M(self, val):
+        self._big_M.value = val
+
+    def _gen_objective(self, X, y):
+        """Generate the quadratic form portion of objective"""
+        # psd_wrap will ignore cvxpy PSD checks, without it errors will
+        # likely be raised since correlation matrices are usually very
+        # poorly conditioned
+        self._z0 = cp.Variable(X.shape[1], boolean=True)
+        c0 = 2 * X.shape[0] # keeps hyperparameter scale independent
+        XTX = psd_wrap(X.T @ X) if self.ignore_psd_check else X.T @ X
+
+        objective = cp.quad_form(self._beta, XTX) - 2 * y.T @ X @ self._beta \
+            + c0 * self._lambda0 * cp.sum(self._z0)
+        return objective
+
+    def _gen_constraints(self, X, y):
+        """Generate the constraints used to solve l0 regularization"""
+        constraints = [self._big_M * self._z0 >= self._beta,
+                       self._big_M * self._z0 >= -self._beta]
+
+        if self.hierarchy is not None:
+            constraints += self._gen_hierarchy_constraints()
+        return constraints
+
+    def _gen_hierarchy_constraints(self):
+        """Generate single feature hierarchy constraints"""
+        return [self._z0[high_id] <= self._z0[sub_id]
+                for high_id, sub_ids in enumerate(self.hierarchy)
+                for sub_id in sub_ids]
+
+
+class MixedL0(RegularizedL0, metaclass=ABCMeta):
     """Abstract base class for mixed L0 regularization models: L1L0 and L2L0.
     """
     def __init__(self, alpha=1.0, l0_ratio=0.5, big_M=1000, hierarchy=None,
@@ -70,9 +174,12 @@ class MixedL0(CVXEstimator, metaclass=ABCMeta):
                 Kewyard arguments passed to cvxpy solve.
                 See docs linked above for more information.
         """
-        super().__init__(fit_intercept=fit_intercept, normalize=normalize,
-                         copy_X=copy_X, warm_start=warm_start, solver=solver,
-                         **kwargs)
+        super().__init__(
+            alpha=alpha, big_M=big_M, hierarchy=hierarchy,
+            ignore_psd_check=ignore_psd_check, fit_intercept=fit_intercept,
+            normalize=normalize, copy_X=copy_X, warm_start=warm_start, solver=solver,
+            **kwargs
+        )
 
         if not 0 <= l0_ratio <= 1:
             raise ValueError('l0_ratio must be between 0 and 1.')
@@ -81,33 +188,16 @@ class MixedL0(CVXEstimator, metaclass=ABCMeta):
                 "It's more efficient to use Ridge/Lasso instead of l0_ratio=0",
                 UserWarning)
 
-        self.hierarchy = hierarchy
-        self._alpha = alpha
-        self._big_M = cp.Parameter(nonneg=True, value=big_M)
-        self._lambda0 = cp.Parameter(nonneg=True, value=l0_ratio * alpha)
+        self._lambda0.value = l0_ratio * alpha
         self._lambda1 = cp.Parameter(nonneg=True, value=(1 - l0_ratio) * alpha)
         #  save exact value so sklearn clone is happy dappy
         self._l0_ratio = l0_ratio
-        self.ignore_psd_check = ignore_psd_check
-        self._z0 = None
 
-    @property
-    def alpha(self):
-        return self._alpha
-
-    @alpha.setter
+    @RegularizedL0.alpha.setter
     def alpha(self, val):
         self._alpha = val
         self._lambda0.value = self.l0_ratio * val
         self._lambda1.value = (1 - self.l0_ratio) * val
-
-    @property
-    def big_M(self):
-        return self._big_M.value
-
-    @big_M.setter
-    def big_M(self, val):
-        self._big_M.value = val
 
     @property
     def l0_ratio(self):
@@ -120,34 +210,6 @@ class MixedL0(CVXEstimator, metaclass=ABCMeta):
         self._l0_ratio = val
         self._lambda0.value = val * self.alpha
         self._lambda1.value = (1 - val) * self.alpha
-
-    def _gen_objective(self, X, y):
-        """Generate the quadratic form portion of objective"""
-        # psd_wrap will ignore cvxpy PSD checks, without it errors will
-        # likely be raised since correlation matrices are usually very
-        # poorly conditioned
-        self._z0 = cp.Variable(X.shape[1], boolean=True)
-        c0 = 2 * X.shape[0] # keeps hyperparameter scale independent
-        XTX = psd_wrap(X.T @ X) if self.ignore_psd_check else X.T @ X
-
-        objective = cp.quad_form(self._beta, XTX) - 2 * y.T @ X @ self._beta \
-            + c0 * self._lambda0 * cp.sum(self._z0)
-        return objective
-
-    def _gen_constraints(self, X, y):
-        """Generate the constraints used to solve l0 regularization"""
-        constraints = [self._big_M * self._z0 >= self._beta,
-                       self._big_M * self._z0 >= -self._beta]
-
-        if self.hierarchy is not None:
-            constraints += self._gen_hierarchy_constraints()
-        return constraints
-
-    def _gen_hierarchy_constraints(self):
-        """Generate single feature hierarchy constraints"""
-        return [self._z0[high_id] <= self._z0[sub_id]
-                for high_id, sub_ids in enumerate(self.hierarchy)
-                for sub_id in sub_ids]
 
 
 class L1L0(MixedL0):
