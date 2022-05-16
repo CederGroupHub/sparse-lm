616ea5a3949ef084d40191e3b89ec641b371a450
lbluque <lbluque@berkeley.edu>
2022-05-15 17:20:38 -0700

    add top level imports for grouped l0

--- sparselm/model/__init__.py
+++ sparselm/model/__init__.py
@@ -3,7 +3,8 @@
 from sparselm.model.ols import OrdinaryLeastSquares
 from sparselm.model.lasso import Lasso
 from sparselm.model.miqp.best_subset import BestSubsetSelection
-from sparselm.model.miqp.regularized_l0 import L1L0, L2L0, RegularizedL0
+from sparselm.model.miqp.regularized_l0 import L1L0, L2L0, RegularizedL0, GroupedL0, \
+    GroupedL2L0
 from sparselm.model.lasso import Lasso, GroupLasso, OverlapGroupLasso, SparseGroupLasso
 from sparselm.model.adaptive_lasso import AdaptiveLasso, AdaptiveGroupLasso, \
     AdaptiveOverlapGroupLasso, AdaptiveSparseGroupLasso
@@ -15,6 +16,8 @@ __all__ = [
     "RegularizedL0",
     "L1L0",
     "L2L0",
+    "GroupedL0",
+    "GroupedL2L0",
     "GroupLasso",
     "OverlapGroupLasso",
     "SparseGroupLasso",
