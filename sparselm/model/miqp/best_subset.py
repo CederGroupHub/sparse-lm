5a0b36e11f9761d6a3ec094206ba4c3863f5702d
lbluque <lbluque@berkeley.edu>
2022-05-16 17:50:59 -0700

    grouped best subset

--- sparselm/model/miqp/best_subset.py
+++ sparselm/model/miqp/best_subset.py
@@ -5,13 +5,12 @@ Allows hierarchy constraints similar to mixed L0 solvers.
 
 __author__ = "Luis Barroso-Luque"
 
-
+import numpy as np
 import cvxpy as cp
 from cvxpy.atoms.affine.wraps import psd_wrap
 from sparselm.model.base import CVXEstimator
 
 
-# TODO implement with l1 and l2 regularization grouped with less slack vars and equality constraints
 class BestSubsetSelection(CVXEstimator):
     """MIQP Best Subset Selection estimator
 
@@ -120,3 +119,78 @@ class BestSubsetSelection(CVXEstimator):
         return [self._z0[high_id] <= self._z0[sub_id]
                 for high_id, sub_ids in enumerate(self.hierarchy)
                 for sub_id in sub_ids]
+
+
+class BestGroupedSubsetSelection(BestSubsetSelection):
+
+    def __init__(self, groups, sparse_bound, big_M=1000, hierarchy=None,
+                 ignore_psd_check=True, fit_intercept=False, normalize=False,
+                 copy_X=True, warm_start=False, solver=None, **kwargs):
+        """
+
+        Args:
+            groups (list or ndarray):
+                array-like of integers specifying groups. Length should be the
+                same as model, where each integer entry specifies the group
+                each parameter corresponds to.
+            sparse_bound (int):
+                Upper bound on sparsity. The upper bound on total number of
+                nonzero coefficients.
+            big_M (float):
+                Upper bound on the norm of coefficients associated with each
+                cluster (groups of coefficients) ||Beta_c||_2
+            hierarchy (list):
+                A list of lists of integers storing hierarchy relations between
+                coefficients.
+                Each sublist contains indices of other coefficients
+                on which the coefficient associated with each element of
+                the list depends. i.e. hierarchy = [[1, 2], [0], []] mean that
+                coefficient 0 depends on 1, and 2; 1 depends on 0, and 2 has no
+                dependence.
+            ignore_psd_check (bool):
+                Wether to ignore cvxpy's PSD checks  of matrix used in quadratic
+                form. Default is True to avoid raising errors for poorly
+                conditioned matrices. But if you want to be strict set to False.
+            fit_intercept (bool):
+                Whether the intercept should be estimated or not.
+                If False, the data is assumed to be already centered.
+            normalize (bool):
+                This parameter is ignored when fit_intercept is set to False.
+                If True, the regressors X will be normalized before regression
+                by subtracting the mean and dividing by the l2-norm.
+                If you wish to standardize, please use StandardScaler before
+                calling fit on an estimator with normalize=False
+            copy_X (bool):
+                If True, X will be copied; else, it may be overwritten.
+            warm_start (bool):
+                When set to True, reuse the solution of the previous call to
+                fit as initialization, otherwise, just erase the previous
+                solution.
+            solver (str):
+                cvxpy backend solver to use. Supported solvers are:
+                ECOS, ECOS_BB, CVXOPT, SCS, GUROBI, Elemental.
+                GLPK and GLPK_MI (via CVXOPT GLPK interface)
+            **kwargs:
+                Kewyard arguments passed to cvxpy solve.
+                See docs linked above for more information.
+        """
+        super().__init__(
+            sparse_bound=sparse_bound, big_M=big_M, hierarchy=hierarchy,
+            ignore_psd_check=ignore_psd_check, fit_intercept=fit_intercept,
+            normalize=normalize, copy_X=copy_X, warm_start=warm_start, solver=solver,
+            **kwargs
+        )
+        self.groups = np.asarray(groups)
+        self._group_masks = [self.groups == i for i in np.unique(groups)]
+        self._z0 = cp.Variable(len(self._group_masks), boolean=True)
+
+    def _gen_constraints(self, X, y):
+        """Generate the constraints used to solve l0 regularization"""
+        constraints = []
+        for i, mask in enumerate(self._group_masks):
+            constraints += [self._big_M * self._z0[i] >= self._beta[mask],
+                            self._big_M * self._z0[i] >= -self._beta[mask]]
+
+        if self.hierarchy is not None:
+            constraints += self._gen_hierarchy_constraints()
+        return constraints
