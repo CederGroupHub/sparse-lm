51223d4dbf8290b67b0755ea40ae40399527afcd
lbluque <lbluque@berkeley.edu>
2023-01-10 19:40:53 -0800

    precommit

--- tests/test_model_selection.py
+++ tests/test_model_selection.py
@@ -8,7 +8,7 @@ import numpy as np
 import pytest
 
 from sparselm.model.miqp._regularized_l0 import L1L0, L2L0
-from sparselm.model_selection import LineSearchCV, GridSearchCV
+from sparselm.model_selection import GridSearchCV, LineSearchCV
 
 ALL_CRITERION = ["max_score", "one_std_score"]
 # Currently we will only test on mixedL0
