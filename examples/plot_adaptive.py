4f34d448ec1a2b0ed2b2456652fcb3cf1d2f18a6
lbluque <lbluque@berkeley.edu>
2023-06-23 15:44:31 -0700

    DOC: examples

---
+++ examples/plot_adaptive.py
@@ -0,0 +1,98 @@
+"""
+==============================
+Using adaptive regularization
+==============================
+
+Adaptive or iteratively re-weighted regularization is a technique that can improve
+feature selection properties over the standard Lasso and Group Lasso extensions. In
+this example we compare the performance of the standard Lasso with adaptive Lasso.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_regression
+from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.model_selection import GridSearchCV, KFold, train_test_split
+from sklearn.linear_model import Lasso
+from sparselm.model import AdaptiveLasso
+
+X, y, coef = make_regression(
+    n_samples=200,
+    n_features=100,
+    n_informative=10,
+    noise=40.0,
+    bias=-15.0,
+    coef=True,
+    random_state=0,
+)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.25, random_state=0
+)
+
+# create estimators
+lasso = Lasso(fit_intercept=True)
+alasso = AdaptiveLasso(max_iter=5, fit_intercept=True)
+
+# create cv search objects for each estimator
+cv5 = KFold(n_splits=5, shuffle=True, random_state=0)
+params = {"alpha": np.logspace(-1, 1, 10)}
+
+gs_lasso = GridSearchCV(lasso, params, cv=cv5, n_jobs=-1)
+gs_alasso = GridSearchCV(alasso, params, cv=cv5, n_jobs=-1)
+
+# fit models on training data
+gs_lasso.fit(X_train, y_train)
+gs_alasso.fit(X_train, y_train)
+
+# calculate model performance on test and train data
+lasso_train = {
+    "r2": r2_score(y_train, gs_lasso.predict(X_train)),
+    "rmse": np.sqrt(mean_squared_error(y_train, gs_lasso.predict(X_train))),
+}
+
+lasso_test = {
+    "r2": r2_score(y_test, gs_lasso.predict(X_test)),
+    "rmse": np.sqrt(mean_squared_error(y_test, gs_lasso.predict(X_test))),
+}
+
+alasso_train = {
+    "r2": r2_score(y_train, gs_alasso.predict(X_train)),
+    "rmse": np.sqrt(mean_squared_error(y_train, gs_alasso.predict(X_train))),
+}
+
+alasso_test = {
+    "r2": r2_score(y_test, gs_alasso.predict(X_test)),
+    "rmse": np.sqrt(mean_squared_error(y_test, gs_alasso.predict(X_test))),
+}
+
+print("Lasso performance metrics:")
+print(f"    train r2: {lasso_train['r2']:.3f}")
+print(f"    test r2: {lasso_test['r2']:.3f}")
+print(f"    train rmse: {lasso_train['rmse']:.3f}")
+print(f"    test rmse: {lasso_test['rmse']:.3f}")
+
+print("Adaptive Lasso performance metrics:")
+print(f"    train r2: {alasso_train['r2']:.3f}")
+print(f"    test r2: {alasso_test['r2']:.3f}")
+print(f"    train rmse: {alasso_train['rmse']:.3f}")
+print(f"    test rmse: {alasso_test['rmse']:.3f}")
+
+# plot predicted values
+fig, ax = plt.subplots()
+ax.plot(y_test, gs_lasso.predict(X_test), "o", label="Lasso", alpha=0.5)
+ax.plot(y_test, gs_alasso.predict(X_test), "o", label="Adaptive Lasso", alpha=0.5)
+ax.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], "k--")
+ax.set_xlabel("True values")
+ax.set_ylabel("Predicted values")
+ax.legend()
+fig.show()
+
+# plot model coefficients
+fig, ax = plt.subplots()
+ax.plot(coef, "o", label="True coefficients")
+ax.plot(gs_lasso.best_estimator_.coef_, "o", label="Lasso", alpha=0.5)
+ax.plot(gs_alasso.best_estimator_.coef_, "o", label="Adaptive Lasso", alpha=0.5)
+ax.set_xlabel("Covariate index")
+ax.set_ylabel("Coefficient value")
+fig.show()
