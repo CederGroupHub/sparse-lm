5857802e8670a04b90b759e4d98fb3f85089ff19
lbluque <lbluque@users.noreply.github.com>
2023-07-25 01:16:35 +0000

    deploy: 0fb4182b721e1b79ec245d447fccb703a7101a4a

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g2a1f8ca',
+    VERSION: '0.1.dev1+g0fb4182',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
